69e65207fe50b57204e1c700782ffa10f72b5c9e
Rio-Hacks <150721118+Rio-Hacks@users.noreply.github.com>
2025-07-21 15:52:35 +0530

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,11 +2,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "JAM Bot is alive!"
+    return "Bot is running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
